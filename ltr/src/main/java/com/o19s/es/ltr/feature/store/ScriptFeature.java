eca829060634d5c2031c9036321a4f0a9afd878f
shenfl <shenfeilong@souche.com>
2019-07-25 17:23:31 +0800

    shenfl add ltr plugin

---
+++ ltr/src/main/java/com/o19s/es/ltr/feature/store/ScriptFeature.java
@@ -0,0 +1,211 @@
+package com.o19s.es.ltr.feature.store;
+
+import com.o19s.es.ltr.LtrQueryContext;
+import com.o19s.es.ltr.feature.Feature;
+import com.o19s.es.ltr.feature.FeatureSet;
+import com.o19s.es.ltr.query.LtrRewritableQuery;
+import com.o19s.es.ltr.ranker.LtrRanker;
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.DocIdSetIterator;
+import org.apache.lucene.search.Explanation;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.Scorer;
+import org.apache.lucene.search.Weight;
+import org.elasticsearch.common.lucene.search.function.LeafScoreFunction;
+import org.elasticsearch.common.lucene.search.function.ScriptScoreFunction;
+import org.elasticsearch.common.xcontent.LoggingDeprecationHandler;
+import org.elasticsearch.common.xcontent.NamedXContentRegistry;
+import org.elasticsearch.common.xcontent.XContentParser;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.script.ScoreScript;
+import org.elasticsearch.script.Script;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+public class ScriptFeature implements Feature {
+    public static final String TEMPLATE_LANGUAGE = "script_feature";
+    public static final String FEATURE_VECTOR = "feature_vector";
+    public static final String EXTRA_SCRIPT_PARAMS = "extra_script_params";
+
+    private final String name;
+    private final Script script;
+    private final Collection<String> queryParams;
+    private final Map<String, Object> baseScriptParams;
+    private final Map<String, String> extraScriptParams;
+
+    @SuppressWarnings("unchecked")
+    public ScriptFeature(String name, Script script, Collection<String> queryParams) {
+        this.name = Objects.requireNonNull(name);
+        this.script = Objects.requireNonNull(script);
+        this.queryParams = queryParams;
+        Map<String, Object> ltrScriptParams = new HashMap<>();
+        Map<String, String> ltrExtraScriptParams = new HashMap<>();
+        for (Map.Entry<String, Object> entry : script.getParams().entrySet()) {
+            if (!entry.getKey().equals(EXTRA_SCRIPT_PARAMS)) {
+                ltrScriptParams.put(String.valueOf(entry.getKey()), entry.getValue());
+            } else {
+                ltrExtraScriptParams = (Map<String, String>) entry.getValue();
+            }
+        }
+        this.baseScriptParams = ltrScriptParams;
+        this.extraScriptParams = ltrExtraScriptParams;
+    }
+
+    public static ScriptFeature compile(StoredFeature feature) {
+        try {
+            XContentParser xContentParser = XContentType.JSON.xContent().createParser(NamedXContentRegistry.EMPTY,
+                    LoggingDeprecationHandler.INSTANCE, feature.template());
+            return new ScriptFeature(feature.name(), Script.parse(xContentParser, "native"), feature.queryParams());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * The feature name
+     */
+    @Override
+    public String name() {
+        return name;
+    }
+
+    /**
+     * Transform this feature into a lucene query
+     */
+    @Override
+    public Query doToQuery(LtrQueryContext context, FeatureSet featureSet, Map<String, Object> params) {
+        List<String> missingParams = queryParams.stream()
+                .filter((x) -> !params.containsKey(x))
+                .collect(Collectors.toList());
+        if (!missingParams.isEmpty()) {
+            String names = String.join(",", missingParams);
+            throw new IllegalArgumentException("Missing required param(s): [" + names + "]");
+        }
+
+        Map<String, Object> queryTimeParams = new HashMap<>();
+        Map<String, Object> extraQueryTimeParams = new HashMap<>();
+        for (String x : queryParams) {
+            if (params.containsKey(x)) {
+                /* If extra_script_param then add the appropriate param name for the script else add name:value as is */
+                if (extraScriptParams.containsKey(x)) {
+                    extraQueryTimeParams.put(extraScriptParams.get(x), params.get(x));
+                } else {
+                    queryTimeParams.put(x, params.get(x));
+                }
+            }
+        }
+
+
+        FeatureSupplier supplier = new FeatureSupplier(featureSet);
+        Map<String, Object> nparams = new HashMap<>();
+        nparams.putAll(baseScriptParams);
+        nparams.putAll(queryTimeParams);
+        nparams.putAll(extraQueryTimeParams);
+        nparams.put(FEATURE_VECTOR, supplier);
+        Script script = new Script(this.script.getType(), this.script.getLang(),
+            this.script.getIdOrCode(), this.script.getOptions(), nparams);
+        ScoreScript.Factory factoryFactory  = context.getQueryShardContext().getScriptService().compile(script, ScoreScript.CONTEXT);
+        ScoreScript.LeafFactory leafFactory = factoryFactory.newFactory(nparams, context.getQueryShardContext().lookup());
+        ScriptScoreFunction function = new ScriptScoreFunction(script, leafFactory);
+        return new LtrScript(function, supplier);
+    }
+
+    static class LtrScript extends Query implements LtrRewritableQuery {
+        private final ScriptScoreFunction function;
+        private final FeatureSupplier supplier;
+        LtrScript(ScriptScoreFunction function, FeatureSupplier supplier) {
+            this.function = function;
+            this.supplier = supplier;
+        }
+
+        @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            LtrScript ol = (LtrScript) o;
+            return sameClassAs(o)
+                    && Objects.equals(function, ol.function);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(classHash(), function);
+        }
+
+        @Override
+        public String toString(String field) {
+            return "LtrScript:" + field;
+        }
+
+        @Override
+        public Weight createWeight(IndexSearcher searcher, boolean needsScores, float boost) throws IOException {
+            if (!needsScores) {
+                return new MatchAllDocsQuery().createWeight(searcher, false, 1F);
+            }
+            return new LtrScriptWeight(this, this.function);
+        }
+
+        @Override
+        public Query ltrRewrite(Supplier<LtrRanker.FeatureVector> vectorSupplier) throws IOException {
+            supplier.set(vectorSupplier);
+            return this;
+        }
+    }
+
+    static class LtrScriptWeight extends Weight {
+        private final ScriptScoreFunction function;
+
+        LtrScriptWeight(Query query, ScriptScoreFunction function) {
+            super(query);
+            this.function = function;
+        }
+
+        @Override
+        public Explanation explain(LeafReaderContext context, int doc) throws IOException {
+            return function.getLeafScoreFunction(context).explainScore(doc, Explanation.noMatch("none"));
+        }
+
+        @Override
+        public Scorer scorer(LeafReaderContext context) throws IOException {
+            LeafScoreFunction leafScoreFunction = function.getLeafScoreFunction(context);
+            DocIdSetIterator iterator = DocIdSetIterator.all(context.reader().maxDoc());
+            return new Scorer(this) {
+                @Override
+                public int docID() {
+                    return iterator.docID();
+                }
+
+                @Override
+                public float score() throws IOException {
+                    return (float) leafScoreFunction.score(iterator.docID(), 0F);
+                }
+
+                @Override
+                public DocIdSetIterator iterator() {
+                    return iterator;
+                }
+            };
+        }
+
+        @Override
+        public void extractTerms(Set<Term> terms) {
+        }
+
+        @Override
+        public boolean isCacheable(LeafReaderContext ctx) {
+            // Never ever cache this query, its parent query is mutable
+            return false;
+        }
+    }
+}
---
+++ ltr/src/test/java/com/o19s/es/ltr/feature/store/StoredLtrModelParserTests.java
@@ -0,0 +1,201 @@
+/*
+ * Copyright [2017] Wikimedia Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.o19s.es.ltr.feature.store;
+
+import com.o19s.es.ltr.ranker.LtrRanker;
+import com.o19s.es.ltr.ranker.linear.LinearRanker;
+import com.o19s.es.ltr.ranker.parser.LtrRankerParserFactory;
+import org.apache.lucene.util.LuceneTestCase;
+import org.elasticsearch.common.ParsingException;
+import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.xcontent.LoggingDeprecationHandler;
+import org.elasticsearch.common.xcontent.ToXContent;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+
+import static org.elasticsearch.common.xcontent.NamedXContentRegistry.EMPTY;
+import static org.elasticsearch.common.xcontent.json.JsonXContent.jsonXContent;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class StoredLtrModelParserTests extends LuceneTestCase {
+    private LtrRanker ranker;
+    private LtrRankerParserFactory factory;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        ranker = new LinearRanker(new float[]{1F,2F,3F});
+        factory = new LtrRankerParserFactory.Builder()
+                .register("model/dummy", () -> (set, model) -> ranker)
+                .build();
+    }
+
+    public String getTestModel() throws IOException {
+        return "{\n" +
+                " \"name\":\"my_model\",\n" +
+                " \"feature_set\":" +
+                StoredFeatureSetParserTests.generateRandomFeatureSet() +
+                "," +
+                " \"model\": {\n" +
+                "   \"type\": \"model/dummy\",\n" +
+                "   \"definition\": \"completely ignored\"\n"+
+                " }" +
+                "}";
+    }
+
+    public String getTestModelAsXContent() throws IOException {
+        return "{\n" +
+                " \"name\":\"my_model\",\n" +
+                " \"feature_set\":" +
+                StoredFeatureSetParserTests.generateRandomFeatureSet() +
+                "," +
+                " \"model\": {\n" +
+                "   \"type\": \"model/dummy\",\n" +
+                "   \"definition\": [\"completely ignored\"]\n"+
+                " }" +
+                "}";
+    }
+
+    public void testParse() throws IOException {
+        StoredLtrModel model = parse(getTestModel());
+        assertTestModel(model);
+    }
+
+    private void assertTestModel(StoredLtrModel model) throws IOException {
+        assertEquals("my_model", model.name());
+        assertEquals("model/dummy", model.rankingModelType());
+        assertEquals("completely ignored", model.rankingModel());
+        assertSame(ranker, model.compile(factory).ranker());
+        assertTrue(model.featureSet().size() > 0);
+    }
+
+    private void assertTestModelAsXContent(StoredLtrModel model) throws IOException {
+        assertEquals("my_model", model.name());
+        assertEquals("model/dummy", model.rankingModelType());
+        assertEquals("[\"completely ignored\"]", model.rankingModel());
+        assertSame(ranker, model.compile(factory).ranker());
+        assertTrue(model.featureSet().size() > 0);
+    }
+
+    public void testToXContent() throws IOException {
+        StoredLtrModel model = parse(getTestModel());
+
+        XContentBuilder builder = XContentFactory.contentBuilder(XContentType.JSON);
+        String modelString = Strings.toString(model.toXContent(builder, ToXContent.EMPTY_PARAMS));
+        StoredLtrModel modelReparsed = parse(modelString);
+        assertTestModel(modelReparsed);
+
+        model = parse(getTestModelAsXContent());
+        builder = XContentFactory.contentBuilder(XContentType.JSON);
+        modelString = Strings.toString(model.toXContent(builder, ToXContent.EMPTY_PARAMS));
+        modelReparsed = parse(modelString);
+        assertTestModelAsXContent(modelReparsed);
+    }
+
+    public void testParseFailureOnMissingName() throws IOException {
+        String modelString = "{\n" +
+                " \"feature_set\":" +
+                StoredFeatureSetParserTests.generateRandomFeatureSet() +
+                "," +
+                " \"model\": {\n" +
+                "   \"type\": \"model/dummy\",\n" +
+                "   \"definition\": \"completely ignored\"\n"+
+                " }" +
+                "}";
+        assertThat(expectThrows(ParsingException.class, () -> parse(modelString)).getMessage(),
+            equalTo("Field [name] is mandatory"));
+    }
+
+    public void testParseWithExternalName() throws IOException {
+        String modelString = "{\n" +
+                " \"feature_set\":" +
+                StoredFeatureSetParserTests.generateRandomFeatureSet() +
+                "," +
+                " \"model\": {\n" +
+                "   \"type\": \"model/dummy\",\n" +
+                "   \"definition\": \"completely ignored\"\n"+
+                " }" +
+                "}";
+        StoredLtrModel model = parse(modelString, "myModel");
+        assertEquals("myModel", model.name());
+    }
+
+    public void testParseWithInconsistentName() throws IOException {
+        String modelString = "{\n" +
+                " \"name\": \"myModel\"," +
+                " \"feature_set\":" +
+                StoredFeatureSetParserTests.generateRandomFeatureSet() +
+                "," +
+                " \"model\": {\n" +
+                "   \"type\": \"model/dummy\",\n" +
+                "   \"definition\": \"completely ignored\"\n"+
+                " }" +
+                "}";
+        assertThat(expectThrows(ParsingException.class, () -> parse(modelString, "myModel2")).getMessage(),
+                equalTo("Invalid [name], expected [myModel2] but got [myModel]"));
+    }
+
+    public void testParseFailureOnMissingModel() throws IOException {
+        String modelString = "{\n" +
+                " \"name\":\"my_model\",\n" +
+                " \"feature_set\":" +
+                StoredFeatureSetParserTests.generateRandomFeatureSet() +
+                "}";
+        assertThat(expectThrows(ParsingException.class, () -> parse(modelString)).getMessage(),
+                equalTo("Field [model] is mandatory"));
+    }
+
+    public void testParseFailureOnMissingFeatureSet() throws IOException {
+        String modelString = "{\n" +
+                " \"name\":\"my_model\",\n" +
+                " \"model\": {\n" +
+                "   \"type\": \"model/dummy\",\n" +
+                "   \"definition\": \"completely ignored\"\n"+
+                " }" +
+                "}";
+        assertThat(expectThrows(ParsingException.class, () -> parse(modelString)).getMessage(),
+                equalTo("Field [feature_set] is mandatory"));
+    }
+
+    public void testParseFailureOnBogusField() throws IOException {
+        String modelString = "{\n" +
+                " \"name\":\"my_model\",\n" +
+                " \"bogusField\": \"foo\",\n" +
+                " \"feature_set\":" +
+                StoredFeatureSetParserTests.generateRandomFeatureSet() +
+                "," +
+                " \"model\": {\n" +
+                "   \"type\": \"model/dummy\",\n" +
+                "   \"definition\": \"completely ignored\"\n"+
+                " }" +
+                "}";
+        assertThat(expectThrows(ParsingException.class, () -> parse(modelString)).getMessage(),
+                containsString("bogusField"));
+    }
+
+    private StoredLtrModel parse(String jsonString) throws IOException {
+        return parse(jsonString, null);
+    }
+
+    private StoredLtrModel parse(String jsonString, String name) throws IOException {
+        return StoredLtrModel.parse(jsonXContent.createParser(EMPTY,
+                LoggingDeprecationHandler.INSTANCE, jsonString), name);
+    }
+}
